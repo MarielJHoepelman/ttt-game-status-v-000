38062dc90fb2720d5b5604d1a8a85652f86c8b59
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-01-05 03:07:14 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -60,11 +60,11 @@ def over?(board)
 end
 
 def winner(board)
- if won?(board).include?("X")  
+ if won?(board).include?("X")
    return "X"
  elsif won?(board).include?("O")
    return "O"
- else 
+ else
    return nil
  end
 end
